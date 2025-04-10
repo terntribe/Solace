9f65960850164beab2782d4ba21fc6759d6a5f07
olukinni_029 <olukinnif`150216029@gmail.com>
2025-04-11 00:44:36 +0100

    new fix

--- src/utils/responses.utils.js
+++
@@ -1,9 +0,0 @@
-const errorResponse = (res, statusCode, error) => {
-    res.status(statusCode).send({status: 'error', error})
-}
-const successResponse = (res, statusCode, message, data) => {
-    res.status(statusCode).send({status: 'success', message: message, data})
-}
-
-
-module.exports = {errorResponse , successResponse};
